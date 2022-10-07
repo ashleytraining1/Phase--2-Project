e763873d666979f5849c807383e848e6f3c08b9e
ashley <ashley.oketo@student.moringaschool.com>
2022-10-07 13:19:16 +0300

    created a menulist file where the data for the images have been proped

--- my-demo/src/helpers/MenuList.js
+++ my-demo/src/helpers/MenuList.js
@@ -1,16 +1,36 @@
 import React from 'react'
 
-function MenuList({food, foodEvent}) {
+function FoodCard({foodata, foodEvent}) {
+  const cardStyles = {
+    width: "250px",
+    height: "300px",
+    margin: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "30px",
+    
+  } 
+
+  const imageStyles = {
+    width: "200px",
+    height: "200px",
+    
+
+  }
+
   return (
     <div  className="ui column">
-    <div
-      className="ui card"
-      key={food.id}
-      onClick={() => foodEvent(food)}
-    >
+    <div className="ui card" style={cardStyles} onClick={() => foodEvent(foodata)}>
+      <img src={foodata.image} style={imageStyles}></img>
+      <h2>{foodata.name}</h2>
+      <h3>{foodata.price}</h3>
+      
+   
+    
      </div>
     </div>
   )
 }
 
-export default MenuList
+export default FoodCard;
