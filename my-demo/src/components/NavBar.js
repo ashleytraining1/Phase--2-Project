007fd5e12c8b06005f9c33a8d8ecaeb8972be152
ashley <ashley.oketo@student.moringaschool.com>
2022-10-07 13:26:03 +0300

    created a navbar and added the necessary styling

--- my-demo/src/components/NavBar.js
+++ my-demo/src/components/NavBar.js
@@ -3,21 +3,22 @@ import {Link} from 'react-router-dom';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import "../styles/NavBar.css";
 
-function NavBar() {   
+function NavBar() { 
+  const navbarStyles = {
+    margin: "auto",
+    width: "1100px",
+    display: "flex",
+    justifyContent: "space-between",
+
+  }
+    
   return (
     <div className='navbar'>
-      {/* <div className='hiddenLinks'> 
-          <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/team"> Team </Link>
-          <Link to="/contact"> Contact </Link>
       
-      </div> */}
         
-        <div className='rightside'>
+        <div className='rightside'style={navbarStyles}>
           <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
+          <Link to="/MenuPages"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/team"> Team </Link>
           <Link to="/contact"> Contact </Link>
